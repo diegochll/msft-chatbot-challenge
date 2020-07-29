01e2b1f61f69fb7f5ddd03c63ab862a090c93892
Diego Chamorro <diegochll@gmail.com>
2020-07-29 15:33:36 -0400

    added relaxation

--- dialogs/covidStatistics.js
+++ dialogs/covidStatistics.js
@@ -1,116 +1,140 @@
+/* eslint-disable */
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { InputHints, MessageFactory } = require('botbuilder');
-const { ChoicePrompt, ComponentDialog, TextPrompt, ConfirmPrompt, WaterfallDialog } = require('botbuilder-dialogs');
-const { AttachmentLayoutTypes, CardFactory } = require('botbuilder');
-const AdaptiveCard = require('../resources/adaptiveCard.json');
+const { InputHints, MessageFactory } = require("botbuilder");
+const {
+  ChoicePrompt,
+  ComponentDialog,
+  TextPrompt,
+  ConfirmPrompt,
+  WaterfallDialog,
+} = require("botbuilder-dialogs");
+const { AttachmentLayoutTypes, CardFactory } = require("botbuilder");
+const AdaptiveCard = require("../resources/adaptiveCard.json");
 
-const COVID_STATISTICS = 'CovidStatistics';
-const WATERFALL_DIALOG = 'waterfallDialog';
+const COVID_STATISTICS = "CovidStatistics";
+const WATERFALL_DIALOG = "waterfallDialog";
 
 class CovidStatistics extends ComponentDialog {
-    constructor(id) {
-        super(id || 'CovidStatistics');
+  constructor(id) {
+    super(id || "CovidStatistics");
 
-        this.addDialog(new TextPrompt('countryPrompt'))
-            .addDialog(new ChoicePrompt('timeFramePrompt'))
-            .addDialog(new ConfirmPrompt('confirmPrompt'))
-            .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
-                this.countryStep.bind(this),
-                this.timeFrameStep.bind(this),
-                this.showDataStep.bind(this),
-                this.finalStep.bind(this),
-            ]));
+    this.addDialog(new TextPrompt("countryPrompt"))
+      .addDialog(new ChoicePrompt("timeFramePrompt"))
+      .addDialog(new ConfirmPrompt("confirmPrompt"))
+      .addDialog(
+        new WaterfallDialog(WATERFALL_DIALOG, [
+          this.countryStep.bind(this),
+          this.timeFrameStep.bind(this),
+          this.showDataStep.bind(this),
+          this.finalStep.bind(this),
+        ])
+      );
 
-        this.initialDialogId = WATERFALL_DIALOG;
+    this.initialDialogId = WATERFALL_DIALOG;
+  }
+
+  /**
+   * If a country has not been provided, prompt for one.
+   */
+  async countryStep(stepContext) {
+    const covidStatisticsDetails = stepContext.options;
+
+    if (!covidStatisticsDetails.country) {
+      const messageText =
+        "From which country do you want to know the statistics?";
+      const msg = MessageFactory.text(
+        messageText,
+        messageText,
+        InputHints.ExpectingInput
+      );
+      return await stepContext.prompt("countryPrompt", { prompt: msg });
+    } else {
+      //LuisVerification
     }
+    return await stepContext.next(covidStatisticsDetails.country);
+  }
 
-    /**
-     * If a country has not been provided, prompt for one.
-     */
-    async countryStep(stepContext) {
-        const covidStatisticsDetails = stepContext.options;
+  /**
+   * If an time frameStep has not been provided, prompt for one.
+   */
+  async timeFrameStep(stepContext) {
+    const covidStatisticsDetails = stepContext.options;
 
-        if (!covidStatisticsDetails.country) {
-            const messageText = 'From which country do you want to know the statistics?';
-            const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
-            return await stepContext.prompt('countryPrompt', { prompt: msg });
-        }else{
-            //LuisVerification
-        }
-        return await stepContext.next(covidStatisticsDetails.country);
+    if (!covidStatisticsDetails.timeFrame) {
+      const options = {
+        prompt:
+          "Which time frame do you want to know? You can click or type the time frame",
+        retryPrompt:
+          "That was not a valid choice, please select a time frame from 1 to 5.",
+        choices: this.getChoices(),
+      };
+      return await stepContext.prompt("timeFramePrompt", options);
     }
+    return await stepContext.next(covidStatisticsDetails.timeFrame);
+  }
 
-    /**
-     * If an time frameStep has not been provided, prompt for one.
-     */
-    async timeFrameStep(stepContext) {
-        const covidStatisticsDetails = stepContext.options;
-        
-        if (!covidStatisticsDetails.timeFrame) {
-            const options = {
-                prompt: 'Which time frame do you want to know? You can click or type the time frame',
-                retryPrompt: 'That was not a valid choice, please select a time frame from 1 to 5.',
-                choices: this.getChoices()
-            };
-            return await stepContext.prompt('timeFramePrompt', options);
-        }
-        return await stepContext.next(covidStatisticsDetails.timeFrame);
+  /**
+   * Complete the interaction and end the dialog.
+   */
+  async showDataStep(stepContext) {
+    const covidStatisticsDetails = stepContext.options;
+
+    await stepContext.context.sendActivity({
+      attachments: [this.createAdaptiveCard()],
+    });
+
+    return await stepContext.prompt(
+      "confirmPrompt",
+      "Do you want to know information about another country?",
+      ["yes", "no"]
+    );
+  }
+
+  async finalStep(stepContext) {
+    // User said "yes"
+    if (stepContext.result) {
+      // Start the covidStatistics dialog.
+      const covidStatisticsDialog = this.findDialog(COVID_STATISTICS);
+      return await stepContext.beginDialog(
+        covidStatisticsDialog.id,
+        covidStatisticsDetails
+      );
     }
+    return await stepContext.endDialog();
+  }
 
-    /**
-     * Complete the interaction and end the dialog.
-     */
-    async showDataStep(stepContext) {
-        const covidStatisticsDetails = stepContext.options;
+  createAdaptiveCard() {
+    return CardFactory.adaptiveCard(AdaptiveCard);
+  }
 
-        await stepContext.context.sendActivity({ attachments: [this.createAdaptiveCard()] });
-        
-        return await stepContext.prompt('confirmPrompt', 'Do you want to know information about another country?', ['yes', 'no']);
-    }
+  getChoices() {
+    const timeFrameOptions = [
+      {
+        value: "Today",
+        synonyms: ["today"],
+      },
+      {
+        value: "Last Week",
+        synonyms: ["last week"],
+      },
+      {
+        value: "Last two Weeks",
+        synonyms: ["last two weeks"],
+      },
+      {
+        value: "Last Month",
+        synonyms: ["last month"],
+      },
+      {
+        value: "Overall",
+        synonyms: ["overall"],
+      },
+    ];
 
-    async finalStep(stepContext) {
-        // User said "yes" 
-        if (stepContext.result) {
-            // Start the covidStatistics dialog.
-            const covidStatisticsDialog = this.findDialog(COVID_STATISTICS);
-            return await stepContext.beginDialog(covidStatisticsDialog.id, covidStatisticsDetails);
-        }   
-        return await stepContext.endDialog();
-    }
-
-    createAdaptiveCard() {
-        return CardFactory.adaptiveCard(AdaptiveCard);
-    }
-
-    getChoices() {
-        const timeFrameOptions = [
-            {
-                value: 'Today',
-                synonyms: ['today']
-            },
-            {
-                value: 'Last Week',
-                synonyms: ['last week']
-            },
-            {
-                value: 'Last two Weeks',
-                synonyms: ['last two weeks']
-            },
-            {
-                value: 'Last Month',
-                synonyms: ['last month']
-            },
-            {
-                value: 'Overall',
-                synonyms: ['overall']
-            }
-        ];
-
-        return timeFrameOptions;
-    }
-
+    return timeFrameOptions;
+  }
 }
 
 module.exports.CovidStatistics = CovidStatistics;
--- dialogs/greet.js
+++ dialogs/greet.js
@@ -1,59 +1,72 @@
+/* eslint-disable */
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { InputHints, MessageFactory } = require('botbuilder');
-const { ChoicePrompt, ComponentDialog, TextPrompt, ConfirmPrompt, WaterfallDialog } = require('botbuilder-dialogs');
-const { AttachmentLayoutTypes, CardFactory } = require('botbuilder');
-const AdaptiveCard = require('../resources/adaptiveCard.json');
-const COVID_STATISTICS = 'CovidStatistics';
-const WATERFALL_DIALOG = 'waterfallDialog';
+const { InputHints, MessageFactory } = require("botbuilder");
+const {
+  ChoicePrompt,
+  ComponentDialog,
+  TextPrompt,
+  ConfirmPrompt,
+  WaterfallDialog,
+} = require("botbuilder-dialogs");
+const { AttachmentLayoutTypes, CardFactory } = require("botbuilder");
+const AdaptiveCard = require("../resources/adaptiveCard.json");
+const COVID_STATISTICS = "CovidStatistics";
+const WATERFALL_DIALOG = "waterfallDialog";
 
 class Greet extends ComponentDialog {
-    constructor(id) {
-        super(id || 'Greet');
+  constructor(id) {
+    super(id || "Greet");
 
-        this.addDialog(new ChoicePrompt('featureOptionsPrompt'))
-            .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
-                this.greetStep.bind(this),
-                this.redirectStep.bind(this),
-            ]));
+    this.addDialog(new ChoicePrompt("featureOptionsPrompt")).addDialog(
+      new WaterfallDialog(WATERFALL_DIALOG, [
+        this.greetStep.bind(this),
+        this.redirectStep.bind(this),
+      ])
+    );
 
-        this.initialDialogId = WATERFALL_DIALOG;
+    this.initialDialogId = WATERFALL_DIALOG;
+  }
+
+  async greetStep(stepContext) {
+    const options = {
+      prompt: "Hello, I am XXX. Which of my features are you interested in?",
+      retryPrompt:
+        "That was not a valid choice, please select a valid feature.",
+      choices: this.getChoices(),
+    };
+    return await stepContext.prompt("featureOptionsPrompt", options);
+  }
+  async redirectStep(stepContext) {
+    switch (stepContext.result.value) {
+      case "Covid Statistics":
+        return stepContext.next("Covid Statistics");
+      case "Relax":
+        return stepContext.next("Relax");
+      default: {
+        return stepContext.endDialog();
+      }
     }
+  }
 
-    async greetStep(stepContext) {
-         const options = {
-            prompt: 'Hello, I am XXX. Which of my features are you interested in?',
-            retryPrompt: 'That was not a valid choice, please select a valid feature.',
-            choices: this.getChoices()
-        };
-        return await stepContext.prompt('featureOptionsPrompt', options);    
-    }
-    async redirectStep(stepContext) {
-        switch (stepContext.result.value) {
-            case 'Covid Statistics':
-                return stepContext.next('Covid Statistics');
-            default: {
-                return stepContext.endDialog();
-            }
-        }
-    }
+  getChoices() {
+    const featuresOptions = [
+      {
+        value: "Covid Statistics",
+      },
+      {
+        value: "Relax",
+        synonyms: ["relax"],
+      },
+      {
+        value: "Hear Good News",
+      },
+    ];
 
-    getChoices() {
-        const featuresOptions = [
-            {
-                value: 'Covid Statistics',
-            },
-            {
-                value: 'Relax',
-            },
-            {
-                value: 'Hear Good News',
-            }
-        ];
-
-        return featuresOptions;
-    }
+    return featuresOptions;
+  }
 }
 
 module.exports.Greet = Greet;
